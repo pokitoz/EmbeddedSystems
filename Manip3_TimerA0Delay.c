744aa19a9092f7ea3ef7f15b953983ce9002e870
aroulin <andy.roulin@epfl.ch>
2014-09-22 18:45:12 +0200

    TimerA0 works with 3*ms

--- Manip3_TimerA0Delay.c
+++ Manip3_TimerA0Delay.c
@@ -15,16 +15,22 @@ void waitFor(int ms) {
 	//We use the divider of the timer to reduce this frequency to 3kHz
 	//So, to wait N ms the timer must count to 3*N.
 
+	// Why is ms << 1 + ms =/= 3*ms ??
+
 	//TIMER A
-	TA1CCR0 = ms << 1 + ms;	// On 16bits. Set the counter. Stop the counter if the value is 0
-	TA1CTL |= MC_1;	// Run the timer
+	TA0CTL |= TACLR;			//Clear register TARCLR
+	TA0CTL |= TASSEL_1 | ID_2;	// Use the ACLK and count UP
+
+	TA0CCTL0 = CCIE;			// Enable timer interruption for TimerA 1
+	TA0CCR0 = 3*ms;	// On 16bits. Set the counter. Stop the counter if the value is 0
+	TA0CTL |= MC_1;	// Run the timer
 
 	waiting = 0x1;
 	while (waiting)
-		P1OUT = BIT0;      //Light on or off the LED RED
+		P1OUT |= BIT0;      //Light on or off the LED RED
 
-	TA1CTL &= ~MC_3;	//Stop the timer
-	P1OUT = 0;
+	TA0CTL &= ~MC_3;	//Stop the timer
+	P1OUT &= ~0x01;
 }
 
 void main(void) {
@@ -37,18 +43,24 @@ void main(void) {
 	//Set the ACLK to VLOCLK
 	BCSCTL3 |= LFXT1S_2;
 
-	TA1CTL |= TACLR;			//Clear register TARCLR
-	TA1CTL |= TASSEL_1 | ID_2;	// Use the ACLK and count UP
+	TA0CTL |= TACLR;			//Clear register TARCLR
+	TA0CTL |= TASSEL_1 | ID_2;	// Use the ACLK and count UP
 
-	TA1CCTL0 = CCIE;			// Enable timer interruption for TimerA 1
-	_BIS_SR(GIE);				// interrupts enabled
+	TA0CCTL0 = CCIE;			// Enable timer interruption for TimerA 1
+	_BIS_SR(GIE);
+	// interrupts enabled
 
-	waitFor(5000);
-	P1OUT = BIT6; 				//Light the greed LED
+	while (1) {
+		P1OUT = BIT6;
+		waitFor(10);
+		P1OUT = 0;
+		waitFor(10);
+	}
+	//Light the greed LED
 
 }
 // Define the interupt routine
-#pragma vector=TIMER0_A1_VECTOR
-__interrupt void Timer0_A1(void) {
+#pragma vector=TIMER0_A0_VECTOR
+__interrupt void Timer0_A0(void) {
 	waiting = 0x0;
 }

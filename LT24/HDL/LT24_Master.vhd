6350f06c4bb50af366a6e342d8d0f9b0021f285e
aroulin <andy.roulin@epfl.ch>
2014-12-03 21:59:53 +0100

    Master: implementation without burst read

--- LT24/HDL/LT24_Master.vhd
+++ LT24/HDL/LT24_Master.vhd
@@ -29,6 +29,59 @@ entity LT24_Master is
 end entity LT24_Master;
 
 architecture RTL of LT24_Master is
+	type state_type is (IDLE, READ_REQUEST, READ_AVAILABLE);
+	signal state_reg, state_next             : state_type;
+	signal address_dma_reg, address_dma_next : std_logic_vector(31 downto 0);
+	signal len_dma_reg, len_dma_next         : unsigned(31 downto 0);
 begin
 	
+	address <= address_dma;
+	
+	state_machine : process(state_reg, address_dma, len_dma, start_dma, fifo_full, len_dma_reg, read_data, wait_request) is
+	begin
+		state_next <= state_reg;
+		running    <= '1';
+		read <= '0';
+		write_fifo <= '1';
+		write_data <= (others => '0');
+		case state_reg is
+			when IDLE =>
+				running <= '0';
+				if (start_dma = '1') then
+					running          <= '1';
+					address_dma_next <= address_dma;
+					len_dma_next     <= unsigned(len_dma);
+					state_next       <= READ_REQUEST;
+				end if;
+			when READ_REQUEST =>
+				read <= '1';
+				if(wait_request = '0') then
+					state_next <= READ_AVAILABLE;
+				end if;
+			when READ_AVAILABLE =>
+				if(fifo_full = '0') then
+					write_fifo <= '1';
+					write_data <= read_data;
+					state_next <= READ_REQUEST;
+					if(len_dma_reg = X"00000001") then
+						state_next <= IDLE;
+					end if;
+					len_dma_next <= len_dma_reg - 1;
+				end if;
+		end case;
+	end process state_machine;
+
+	reg_process : process(clk, reset_n) is
+	begin
+		if reset_n = '0' then
+			state_reg       <= IDLE;
+			address_dma_reg <= (others => '0');
+			len_dma_reg     <= (others => '0');
+		elsif rising_edge(clk) then
+			state_reg       <= state_next;
+			address_dma_reg <= address_dma_next;
+			len_dma_reg     <= len_dma_next;
+		end if;
+	end process reg_process;
+
 end architecture RTL;

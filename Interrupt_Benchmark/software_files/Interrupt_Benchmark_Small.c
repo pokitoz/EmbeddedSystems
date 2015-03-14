fa56350ca09eb51b9feca344c0a97abbc158d1cd
Andy Roulin <andy.roulin@epfl.ch>
2015-03-14 13:49:24 +0100

    Recovery time measurement
    Can optimise a bit when reading the recovery timer

--- Interrupt_Benchmark/software_files/Interrupt_Benchmark_Small.c
+++ Interrupt_Benchmark/software_files/Interrupt_Benchmark_Small.c
@@ -1,14 +1,13 @@
 #include "sys/alt_stdio.h"      // alt_printf
-#include "system.h"             // system memory map constants
-
+#include "system.h"             // system memory map constants
 #include "leds.h"               // leds API
-#include "alt_timer.h"          // Altera timer API
-
+#include "alt_timer.h"          // Altera timer API
 #include "altera_avalon_timer_regs.h"   // Altera timer low-level API
 
 #define FIVE_SECONDS 250000000
-#define ONE_MILLISECOND 50000
-#define RESPONSE_TIME_PERIOD ONE_MILLISECOND
+#define TEN_MILLISECONDS 500000
+#define RESPONSE_TIME_PERIOD TEN_MILLISECONDS
+#define RECOVERY_TIME_PERIOD TEN_MILLISECONDS
 
 const struct alt_timer timer0 = { .base = TIMER0_BASE, .irq_ctrl_id =
 TIMER0_IRQ_INTERRUPT_CONTROLLER_ID, .irq_no = TIMER0_IRQ };
@@ -36,24 +35,44 @@ void isr_timer0(void* context)
     leds(1);
     // Clear irq flag TO
     alt_timer_clr_irq(&timer0);
+
+    // recovery time: start timer1
+    alt_timer_reset(&timer1);
+    alt_timer_start(&timer1);
 }
 
 int main(void)
 {
     alt_putstr("Interrupt Benchmarking Go!\n");
 
+    double recovery_time_avg = 0;
+    alt_u32 recovery_time_cnt = 0;
+
     alt_timer_init(&timer0, RESPONSE_TIME_PERIOD, true, isr_timer0);
+    alt_timer_init(&timer1, RECOVERY_TIME_PERIOD, false, 0);
     alt_timer_start(&timer0);
 
     /* Event loop never exits. */
     while (1) {
         leds(0);
+        // block while recovery timer has not started
+        while(alt_timer_read(&timer1) == RECOVERY_TIME_PERIOD);
+
+        // Recovery time measurement and cumulative average
+        alt_timer_stop(&timer1);
+        alt_u32 recovery_time = RECOVERY_TIME_PERIOD - alt_timer_read(&timer1);
+        recovery_time_avg = (recovery_time + recovery_time_cnt * recovery_time_avg)
+                / (recovery_time_cnt + 1);
+        recovery_time_cnt++;
+
+        alt_timer_reset(&timer1);
         if(response_time_cnt >= 10)
             break;
     }
 
     alt_timer_stop(&timer0);
     alt_printf("Response Time: #0x%x:0x%x\n", response_time_cnt, (int) response_time_avg);
+    alt_printf("Recovery Time: #0x%x:0x%x\n", recovery_time_cnt, (int) recovery_time_avg);
 
     // Dead end
     while(1) {

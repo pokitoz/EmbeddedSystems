33bb9c09803325c9be97fadc05ac2c3bc5e74e5a
Andy Roulin <andy.roulin@epfl.ch>
2015-03-14 12:28:41 +0100

    Altera Interval Timer API

--- Interrupt_Benchmark/software_files/Interrupt_Benchmark_Small.c
+++ Interrupt_Benchmark/software_files/Interrupt_Benchmark_Small.c
@@ -1,25 +1,36 @@
 #include "sys/alt_stdio.h"
-#include "sys/unistd.h"
+#include "system.h"
+
 #include "leds.h"
+#include "alt_timer.h"
+
+#define FIVE_SECONDS 250000000
+
+const struct alt_timer timer0 = { .base = TIMER0_BASE, .irq_ctrl_id =
+        TIMER0_IRQ_INTERRUPT_CONTROLLER_ID, .irq_no = TIMER0_IRQ };
+
+const struct alt_timer timer1 = { .base = TIMER1_BASE, .irq_ctrl_id =
+        TIMER1_IRQ_INTERRUPT_CONTROLLER_ID, .irq_no = TIMER1_IRQ };
+
+void isr_timer0(void* context)
+{
+    leds(1);
+    alt_putchar('o');
+    // Clear irq flag TO
+    alt_timer_clr_irq(&timer0);
+}
 
 int main(void)
-{ 
-  alt_putstr("Interrupt Benchmarking Go!\n");
-  leds(1);
+{
+    alt_putstr("Interrupt Benchmarking Go!\n");
 
-  /* Event loop never exits. */
-  while (1) {
-  		int i = 0;
-  		for (i = 1; i < 6; ++i) {
-  			leds(1 << i);
-  			usleep(200000 / 8);
-  		}
+    alt_timer_init(&timer0, FIVE_SECONDS, true, isr_timer0);
+    alt_timer_start(&timer0);
 
-  		for (i = 4; i >= 0; --i) {
-  			leds(1 << i);
-  			usleep(200000 / 8);
-  		}
-  	}
+    /* Event loop never exits. */
+    while (1) {
+        leds(0);
+    }
 
-  return 0;
+    return 0;
 }

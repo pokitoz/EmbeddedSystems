a87ddd69b35037c56e0a12c7bd72b6b63b699771
Andy Roulin <andy.roulin@epfl.ch>
2015-03-27 13:25:44 +0100

    Using semaphore and buttons to wake up a task
    * Still need to distinguish between rising and falling edges
    * Note how the ISR checks if someone is waiting on the semaphore

--- RTOS_Benchmark/software_files/hello_ucosii.c
+++ RTOS_Benchmark/software_files/hello_ucosii.c
@@ -1,119 +1,53 @@
-/*************************************************************************
-* Copyright (c) 2004 Altera Corporation, San Jose, California, USA.      *
-* All rights reserved. All use of this software and documentation is     *
-* subject to the License Agreement located at the end of this file below.*
-**************************************************************************
-* Description:                                                           *
-* The following is a simple hello world program running MicroC/OS-II.The * 
-* purpose of the design is to be a very simple application that just     *
-* demonstrates MicroC/OS-II running on NIOS II.The design doesn't account*
-* for issues such as checking system call return codes. etc.             *
-*                                                                        *
-* Requirements:                                                          *
-*   -Supported Example Hardware Platforms                                *
-*     Standard                                                           *
-*     Full Featured                                                      *
-*     Low Cost                                                           *
-*   -Supported Development Boards                                        *
-*     Nios II Development Board, Stratix II Edition                      *
-*     Nios Development Board, Stratix Professional Edition               *
-*     Nios Development Board, Stratix Edition                            *
-*     Nios Development Board, Cyclone Edition                            *
-*   -System Library Settings                                             *
-*     RTOS Type - MicroC/OS-II                                           *
-*     Periodic System Timer                                              *
-*   -Know Issues                                                         *
-*     If this design is run on the ISS, terminal output will take several*
-*     minutes per iteration.                                             *
-**************************************************************************/
-
-
 #include <stdio.h>
 #include "includes.h"
+#include "altera_avalon_pio_regs.h"
 
 /* Definition of Task Stacks */
 #define   TASK_STACKSIZE       2048
-OS_STK    task1_stk[TASK_STACKSIZE];
-OS_STK    task2_stk[TASK_STACKSIZE];
+OS_STK sem_task_stk[TASK_STACKSIZE];
 
 /* Definition of Task Priorities */
+#define SEM_TASK_PRIORITY      1
 
-#define TASK1_PRIORITY      1
-#define TASK2_PRIORITY      2
-
-/* Prints "Hello World" and sleeps for three seconds */
-void task1(void* pdata)
-{
-  while (1)
-  { 
-    printf("Hello from task1\n");
-    OSTimeDlyHMSM(0, 0, 3, 0);
-  }
-}
-/* Prints "Hello World" and sleeps for three seconds */
-void task2(void* pdata)
-{
-  while (1)
-  { 
-    printf("Hello from task2\n");
-    OSTimeDlyHMSM(0, 0, 3, 0);
-  }
-}
-/* The main function creates two task and starts multi-tasking */
-int main(void)
-{
-  
-  OSTaskCreateExt(task1,
-                  NULL,
-                  (void *)&task1_stk[TASK_STACKSIZE-1],
-                  TASK1_PRIORITY,
-                  TASK1_PRIORITY,
-                  task1_stk,
-                  TASK_STACKSIZE,
-                  NULL,
-                  0);
-              
-               
-  OSTaskCreateExt(task2,
-                  NULL,
-                  (void *)&task2_stk[TASK_STACKSIZE-1],
-                  TASK2_PRIORITY,
-                  TASK2_PRIORITY,
-                  task2_stk,
-                  TASK_STACKSIZE,
-                  NULL,
-                  0);
-  OSStart();
-  return 0;
+/* A synchronization semaphore suspends a task until a falling edge
+ *  is detected on Button_n[0] */
+OS_EVENT* sem_task_sem;
+INT8U sem_task_sem_err;
+void sem_task(void* pdata) {
+	while (1) {
+		OSSemPend(sem_task_sem, 0, &sem_task_sem_err);
+		printf("sem_task: Button_n[0] was pressed\n");
+	}
 }
 
-/******************************************************************************
-*                                                                             *
-* License Agreement                                                           *
-*                                                                             *
-* Copyright (c) 2004 Altera Corporation, San Jose, California, USA.           *
-* All rights reserved.                                                        *
-*                                                                             *
-* Permission is hereby granted, free of charge, to any person obtaining a     *
-* copy of this software and associated documentation files (the "Software"),  *
-* to deal in the Software without restriction, including without limitation   *
-* the rights to use, copy, modify, merge, publish, distribute, sublicense,    *
-* and/or sell copies of the Software, and to permit persons to whom the       *
-* Software is furnished to do so, subject to the following conditions:        *
-*                                                                             *
-* The above copyright notice and this permission notice shall be included in  *
-* all copies or substantial portions of the Software.                         *
-*                                                                             *
-* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR  *
-* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,    *
-* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE *
-* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER      *
-* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING     *
-* FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER         *
-* DEALINGS IN THE SOFTWARE.                                                   *
-*                                                                             *
-* This agreement shall be governed in all respects by the laws of the State   *
-* of California and by the laws of the United States of America.              *
-* Altera does not recommend, suggest or require that this reference design    *
-* file be used in conjunction or combination with any other product.          *
-******************************************************************************/
+void isr_buttons(void* context) {
+	OS_SEM_DATA sem_task_sem_data;
+	OSSemQuery(sem_task_sem, &sem_task_sem_data);
+	if(sem_task_sem_data.OSEventGrp > 0)
+		OSSemPost(sem_task_sem);
+	// Clear All IRQ
+	IOWR_ALTERA_AVALON_PIO_EDGE_CAP(INPUTS_BASE, 0xF);
+}
+
+int main(void) {
+
+	sem_task_sem = OSSemCreate(0);
+
+	OSTaskCreateExt(sem_task,
+			NULL,
+			(void *) &sem_task_stk[TASK_STACKSIZE - 1],
+			SEM_TASK_PRIORITY,
+			SEM_TASK_PRIORITY,
+			sem_task_stk,
+			TASK_STACKSIZE,
+			NULL,
+			0);
+
+	// Enable Buttons' IRQ
+	IOWR_ALTERA_AVALON_PIO_IRQ_MASK(INPUTS_BASE, 0xF);
+	alt_ic_isr_register(INPUTS_IRQ_INTERRUPT_CONTROLLER_ID,
+			INPUTS_IRQ, isr_buttons, 0, 0);
+
+	OSStart();
+	return 0;
+}

36c87f517618984ff1d85027fead6ec3057fbd35
Andy Roulin <andy.roulin@epfl.ch>
2015-04-23 23:09:48 +0200

    results no burst

--- SDRAM_Analysis/software/main.c
+++ SDRAM_Analysis/software/main.c
@@ -18,15 +18,33 @@
 #include <inttypes.h>
 #include "system.h"
 
+#define WRITE
+
 int main(void)
 {
     printf("Hello from Nios II (SDRAM_Analysis)!\n");
 
-    uint32_t *ptr = SDRAM_BASE;
+    volatile uint8_t *ptr = SDRAM_BASE;
+    volatile uint16_t var = 0;
 
-    int i = 0;
+    uint8_t i = 0;
     for (i = 0; i < 10; ++i) {
-        *ptr++ = i;
+#ifdef READ
+        var = *ptr;
+        ptr += 1024;
+#else if WRITE
+        *ptr++ = i+0xCA;
+#endif
+    }
+
+    ptr = (uint32_t *)(SDRAM_BASE + 512);
+    for (i = 0; i < 10; ++i) {
+#ifdef READ
+        var = *ptr;
+        ptr += 1024;
+#else if WRITE
+        *ptr++ = i+0xCA;
+#endif
     }
 
     printf("Done");

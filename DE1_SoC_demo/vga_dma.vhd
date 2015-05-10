277aca849886f03c5f3a15df43827cf216a152ad
Andy Roulin <andy.roulin@epfl.ch>
2015-05-10 15:06:55 +0200

    vga dma

--- DE1_SoC_demo/vga_dma.vhd
+++ DE1_SoC_demo/vga_dma.vhd
@@ -3,32 +3,108 @@ use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
 entity vga_dma is
-    port(
-        clk                 : in  std_logic;
-        rst_n               : in  std_logic;
+	port(
+		clk                 : in  std_logic;
+		rst_n               : in  std_logic;
 
-        -- Image Buffer in memory
-        framebuffer_address : in  std_logic_vector(31 downto 0);
-        framebuffer_set     : in  std_logic;
+		-- Image Buffer in memory
+		framebuffer_address : in  std_logic_vector(31 downto 0);
+		framebuffer_set     : in  std_logic;
 
-        -- signal from vga_controller
-        dma_start_fetching  : in  std_logic;
+		-- signal from vga_controller
+		dma_start_fetching  : in  std_logic;
 
-        -- FIFO signals
-        fifo_clr            : out std_logic;
-        fifo_data           : out std_logic_vector(31 downto 0);
-        fifo_write          : out std_logic;
-        fifo_full           : out std_logic;
-        fifo_used           : out std_logic_vector(7 downto 0);
+		-- FIFO signals
+		fifo_clr            : out std_logic;
+		fifo_data           : out std_logic_vector(31 downto 0);
+		fifo_write          : out std_logic;
+		fifo_half_full      : in  std_logic;
 
-        -- Avalon 32-bit Master Interface (Read DMA)
-        -- Maximum read burst length is 2^(11-1) = 1024
-        am_address          : out std_logic_vector(31 downto 0);
-        am_byteenable       : out std_logic_vector(3 downto 0);
-        am_read             : out std_logic;
-        am_burstcount       : out std_logic_vector(10 downto 0);
-        am_readdata         : in  std_logic_vector(31 downto 0);
-        am_waitrequest      : in  std_logic;
-        am_readdatavalid    : in  std_logic
-    );
+		-- Avalon 32-bit Master Interface (Read DMA)
+		-- Maximum read burst length is 2^(11-1) = 1024
+		am_address          : out std_logic_vector(31 downto 0);
+		am_byteenable       : out std_logic_vector(3 downto 0);
+		am_read             : out std_logic;
+		am_burstcount       : out std_logic_vector(10 downto 0);
+		am_readdata         : in  std_logic_vector(31 downto 0);
+		am_waitrequest      : in  std_logic;
+		am_readdatavalid    : in  std_logic
+	);
 end entity vga_dma;
+
+architecture rtl of vga_dma is
+	type state_type is (IDLE, START_BURST, IN_BURST);
+	signal state_reg, state_next                             : state_type;
+	signal remaining_reg, remaining_next                     : integer;
+	signal pixels_done_reg, pixels_done_next                 : std_logic_vector(31 downto 0);
+	signal framebuffer_address_reg, framebuffer_address_next : unsigned(31 downto 0);
+	constant BURST_SIZE                                      : integer := 32;
+	signal burst_remaining_reg, burst_remaining_next         : integer;
+
+begin
+	framebuffer_address_next <= unsigned(framebuffer_address) when framebuffer_set = '1' else framebuffer_address_reg;
+
+	state_machine : process(state_reg, am_readdatavalid, am_waitrequest, burst_remaining_reg, dma_start_fetching, fifo_half_full, framebuffer_address_reg, pixels_done_reg, pixels_done_reg(29 downto 0), remaining_reg, am_readdata) is
+	begin
+		fifo_clr   <= '0';
+		fifo_write <= '0';
+		fifo_data  <= (others => '0');
+
+		am_address    <= (others => '0');
+		am_burstcount <= (others => '0');
+		am_byteenable <= "1111";
+		am_read       <= '0';
+
+		state_next <= state_reg;
+
+		case state_reg is
+			when IDLE =>
+				if (dma_start_fetching = '1') then
+					state_next <= START_BURST;
+					fifo_clr   <= '1';
+				end if;
+			when START_BURST =>
+				if (remaining_reg = 0) then
+					state_next <= IDLE;
+				elsif (fifo_half_full = '0') then
+					am_read              <= '1';
+					am_burstcount        <= std_logic_vector(to_unsigned(BURST_SIZE, am_burstcount'range));
+					am_address           <= std_logic_vector(framebuffer_address_reg + unsigned((pixels_done_reg(29 downto 0) & "00")));
+					burst_remaining_next <= BURST_SIZE;
+					if (am_waitrequest = '0') then
+						state_next <= IN_BURST;
+					end if;
+				end if;
+			when IN_BURST =>
+				if (burst_remaining_reg = 0) then
+					state_next <= START_BURST;
+				elsif (am_readdatavalid = '1') then
+					burst_remaining_next <= burst_remaining_reg - 1;
+					remaining_next       <= remaining_reg - 1;
+					pixels_done_next     <= std_logic_vector(unsigned(pixels_done_reg) + 1);
+					burst_remaining_next <= burst_remaining_reg - 1;
+					fifo_data            <= am_readdata;
+					fifo_write           <= '1';
+				end if;
+		end case;
+	end process state_machine;
+
+	pr_reg : process(clk, rst_n) is
+	begin
+		if rst_n = '0' then
+			state_reg               <= IDLE;
+			remaining_reg           <= 0;
+			pixels_done_reg         <= (others => '0');
+			burst_remaining_reg     <= 0;
+			framebuffer_address_reg <= (others => '0');
+		elsif rising_edge(clk) then
+			state_reg               <= state_next;
+			remaining_reg           <= remaining_next;
+			pixels_done_reg         <= pixels_done_next;
+			burst_remaining_reg     <= burst_remaining_next;
+			framebuffer_address_reg <= framebuffer_address_next;
+		end if;
+	end process pr_reg;
+
+end architecture rtl;
+
--- DE1_SoC_demo/vga_module.vhd
+++ DE1_SoC_demo/vga_module.vhd
@@ -9,203 +9,201 @@ use ieee.numeric_std.all;
 --     010: Vsync irq, read to assert (R)
 
 entity vga_module is
-    port(
-        clk              : in  std_logic;
-        rst_n            : in  std_logic;
+	port(
+		clk              : in  std_logic;
+		rst_n            : in  std_logic;
 
-        -- 25.175 Pixel Clock for VGA Controller
-        pixel_clk        : in  std_logic;
+		-- 25.175 Pixel Clock for VGA Controller
+		pixel_clk        : in  std_logic;
 
-        -- Vsync Interrupt
-        vsync_irq        : out std_logic;
+		-- Vsync Interrupt
+		vsync_irq        : out std_logic;
 
-        -- Output to VGA DAC
-        vga_r            : out std_logic_vector(7 downto 0);
-        vga_g            : out std_logic_vector(7 downto 0);
-        vga_b            : out std_logic_vector(7 downto 0);
-        vga_clk          : out std_logic;
-        vga_sync_n       : out std_logic;
-        vga_blank_n      : out std_logic;
+		-- Output to VGA DAC
+		vga_r            : out std_logic_vector(7 downto 0);
+		vga_g            : out std_logic_vector(7 downto 0);
+		vga_b            : out std_logic_vector(7 downto 0);
+		vga_clk          : out std_logic;
+		vga_sync_n       : out std_logic;
+		vga_blank_n      : out std_logic;
 
-        -- Direct Output to VGA Connector
-        vga_vs           : out std_logic;
-        vga_hs           : out std_logic;
+		-- Direct Output to VGA Connector
+		vga_vs           : out std_logic;
+		vga_hs           : out std_logic;
 
-        -- Avalon 32-bit Slave Interface
-        -- 8 memory-mapped registers
-        as_address       : in  std_logic_vector(2 downto 0);
-        as_byteenable    : in  std_logic_vector(3 downto 0);
-        as_read          : in  std_logic;
-        as_readdata      : out std_logic_vector(31 downto 0);
-        as_write         : in  std_logic;
-        as_writedata     : in  std_logic_vector(31 downto 0);
+		-- Avalon 32-bit Slave Interface
+		-- 8 memory-mapped registers
+		as_address       : in  std_logic_vector(2 downto 0);
+		as_byteenable    : in  std_logic_vector(3 downto 0);
+		as_read          : in  std_logic;
+		as_readdata      : out std_logic_vector(31 downto 0);
+		as_write         : in  std_logic;
+		as_writedata     : in  std_logic_vector(31 downto 0);
 
-        -- Avalon 32-bit Master Interface (Read DMA)
-        -- Maximum read burst length is 2^(11-1) = 1024
-        am_address       : out std_logic_vector(31 downto 0);
-        am_byteenable    : out std_logic_vector(3 downto 0);
-        am_read          : out std_logic;
-        am_burstcount    : out std_logic_vector(10 downto 0);
-        am_readdata      : in  std_logic_vector(31 downto 0);
-        am_waitrequest   : in  std_logic;
-        am_readdatavalid : in  std_logic
-    );
+		-- Avalon 32-bit Master Interface (Read DMA)
+		-- Maximum read burst length is 2^(11-1) = 1024
+		am_address       : out std_logic_vector(31 downto 0);
+		am_byteenable    : out std_logic_vector(3 downto 0);
+		am_read          : out std_logic;
+		am_burstcount    : out std_logic_vector(10 downto 0);
+		am_readdata      : in  std_logic_vector(31 downto 0);
+		am_waitrequest   : in  std_logic;
+		am_readdatavalid : in  std_logic
+	);
 end entity vga_module;
 
 architecture structural of vga_module is
-    component vga_slave
-        port(clk                 : in  std_logic;
-             rst_n               : in  std_logic;
-             vsync_irq           : out std_logic;
-             framebuffer_address : out std_logic_vector(31 downto 0);
-             framebuffer_set     : out std_logic;
-             vga_start           : out std_logic;
-             vga_stop            : out std_logic;
-             vga_vsync           : in  std_logic;
-             as_address          : in  std_logic_vector(2 downto 0);
-             as_byteenable       : in  std_logic_vector(3 downto 0);
-             as_read             : in  std_logic;
-             as_readdata         : out std_logic_vector(31 downto 0);
-             as_write            : in  std_logic;
-             as_writedata        : in  std_logic_vector(31 downto 0));
-    end component vga_slave;
+	component vga_slave
+		port(clk                 : in  std_logic;
+			 rst_n               : in  std_logic;
+			 vsync_irq           : out std_logic;
+			 framebuffer_address : out std_logic_vector(31 downto 0);
+			 framebuffer_set     : out std_logic;
+			 vga_start           : out std_logic;
+			 vga_stop            : out std_logic;
+			 vga_vsync           : in  std_logic;
+			 as_address          : in  std_logic_vector(2 downto 0);
+			 as_byteenable       : in  std_logic_vector(3 downto 0);
+			 as_read             : in  std_logic;
+			 as_readdata         : out std_logic_vector(31 downto 0);
+			 as_write            : in  std_logic;
+			 as_writedata        : in  std_logic_vector(31 downto 0));
+	end component vga_slave;
 
-    signal slave_to_dma_framebuffer_address : std_logic_vector(31 downto 0);
-    signal slave_to_dma_framebuffer_set     : std_logic;
+	signal slave_to_dma_framebuffer_address : std_logic_vector(31 downto 0);
+	signal slave_to_dma_framebuffer_set     : std_logic;
 
-    signal slave_to_controller_start        : std_logic;
-    signal slave_to_controller_stop         : std_logic;
-    signal controller_to_slave_vsync        : std_logic;
-    signal controller_to_dma_start_fetching : std_logic;
+	signal slave_to_controller_start        : std_logic;
+	signal slave_to_controller_stop         : std_logic;
+	signal controller_to_slave_vsync        : std_logic;
+	signal controller_to_dma_start_fetching : std_logic;
 
-    signal dma_to_fifo_clr   : std_logic;
-    signal dma_to_fifo_data  : std_logic_vector(31 downto 0);
-    signal dma_to_fifo_write : std_logic;
-    signal fifo_to_dma_full  : std_logic;
-    signal fifo_to_dma_used  : std_logic_vector(7 downto 0);
+	signal dma_to_fifo_clr   : std_logic;
+	signal dma_to_fifo_data  : std_logic_vector(31 downto 0);
+	signal dma_to_fifo_write : std_logic;
+	signal fifo_to_dma_full  : std_logic;
+	signal fifo_to_dma_used  : std_logic_vector(5 downto 0);
 
-    signal fifo_to_controller_data  : std_logic_vector(31 downto 0);
-    signal fifo_to_controller_empty : std_logic;
-    signal controller_to_fifo_read  : std_logic;
+	signal fifo_to_controller_data  : std_logic_vector(31 downto 0);
+	signal fifo_to_controller_empty : std_logic;
+	signal controller_to_fifo_read  : std_logic;
 
-    component vga_dma
-        port(clk                 : in  std_logic;
-             rst_n               : in  std_logic;
-             framebuffer_address : in  std_logic_vector(31 downto 0);
-             framebuffer_set     : in  std_logic;
-             dma_start_fetching  : in  std_logic;
-             fifo_clr            : out std_logic;
-             fifo_data           : out std_logic_vector(31 downto 0);
-             fifo_write          : out std_logic;
-             fifo_full           : out std_logic;
-             fifo_used           : out std_logic_vector(7 downto 0);
-             am_address          : out std_logic_vector(31 downto 0);
-             am_byteenable       : out std_logic_vector(3 downto 0);
-             am_read             : out std_logic;
-             am_burstcount       : out std_logic_vector(10 downto 0);
-             am_readdata         : in  std_logic_vector(31 downto 0);
-             am_waitrequest      : in  std_logic;
-             am_readdatavalid    : in  std_logic);
-    end component vga_dma;
+	component vga_dma
+		port(clk                 : in  std_logic;
+			 rst_n               : in  std_logic;
+			 framebuffer_address : in  std_logic_vector(31 downto 0);
+			 framebuffer_set     : in  std_logic;
+			 dma_start_fetching  : in  std_logic;
+			 fifo_clr            : out std_logic;
+			 fifo_data           : out std_logic_vector(31 downto 0);
+			 fifo_write          : out std_logic;
+			 fifo_half_full      : in  std_logic;
+			 am_address          : out std_logic_vector(31 downto 0);
+			 am_byteenable       : out std_logic_vector(3 downto 0);
+			 am_read             : out std_logic;
+			 am_burstcount       : out std_logic_vector(10 downto 0);
+			 am_readdata         : in  std_logic_vector(31 downto 0);
+			 am_waitrequest      : in  std_logic;
+			 am_readdatavalid    : in  std_logic);
+	end component vga_dma;
 
-    component vga_fifo
-        port(aclr    : IN  STD_LOGIC := '0';
-             data    : IN  STD_LOGIC_VECTOR(31 DOWNTO 0);
-             rdclk   : IN  STD_LOGIC;
-             rdreq   : IN  STD_LOGIC;
-             wrclk   : IN  STD_LOGIC;
-             wrreq   : IN  STD_LOGIC;
-             q       : OUT STD_LOGIC_VECTOR(31 DOWNTO 0);
-             rdempty : OUT STD_LOGIC;
-             wrfull  : OUT STD_LOGIC;
-             wrusedw : OUT STD_LOGIC_VECTOR(7 DOWNTO 0));
-    end component vga_fifo;
+	component vga_fifo
+		port(aclr    : IN  STD_LOGIC := '0';
+			 data    : IN  STD_LOGIC_VECTOR(31 DOWNTO 0);
+			 rdclk   : IN  STD_LOGIC;
+			 rdreq   : IN  STD_LOGIC;
+			 wrclk   : IN  STD_LOGIC;
+			 wrreq   : IN  STD_LOGIC;
+			 q       : OUT STD_LOGIC_VECTOR(31 DOWNTO 0);
+			 rdempty : OUT STD_LOGIC;
+			 wrfull  : OUT STD_LOGIC;
+			 wrusedw : OUT STD_LOGIC_VECTOR(5 DOWNTO 0));
+	end component vga_fifo;
 
-    component vga_controller
-        port(pixel_clk          : in  std_logic;
-             rst_n              : in  std_logic;
-             start              : in  std_logic;
-             stop               : in  std_logic;
-             dma_start_fetching : out std_logic;
-             vsync              : out std_logic;
-             fifo_empty         : in  std_logic;
-             fifo_read          : out std_logic;
-             fifo_data          : in  std_logic_vector(31 downto 0);
-             vga_r              : out std_logic_vector(7 downto 0);
-             vga_g              : out std_logic_vector(7 downto 0);
-             vga_b              : out std_logic_vector(7 downto 0);
-             vga_clk            : out std_logic;
-             vga_sync_n         : out std_logic;
-             vga_blank_n        : out std_logic;
-             vga_vs             : out std_logic;
-             vga_hs             : out std_logic);
-    end component vga_controller;
+	component vga_controller
+		port(pixel_clk          : in  std_logic;
+			 rst_n              : in  std_logic;
+			 start              : in  std_logic;
+			 stop               : in  std_logic;
+			 dma_start_fetching : out std_logic;
+			 vsync              : out std_logic;
+			 fifo_empty         : in  std_logic;
+			 fifo_read          : out std_logic;
+			 fifo_data          : in  std_logic_vector(31 downto 0);
+			 vga_r              : out std_logic_vector(7 downto 0);
+			 vga_g              : out std_logic_vector(7 downto 0);
+			 vga_b              : out std_logic_vector(7 downto 0);
+			 vga_clk            : out std_logic;
+			 vga_sync_n         : out std_logic;
+			 vga_blank_n        : out std_logic;
+			 vga_vs             : out std_logic;
+			 vga_hs             : out std_logic);
+	end component vga_controller;
 
 begin
-    vga_slave_inst : component vga_slave
-        port map(clk                 => clk,
-                 rst_n               => rst_n,
-                 vsync_irq           => vsync_irq,
-                 framebuffer_address => slave_to_dma_framebuffer_address,
-                 framebuffer_set     => slave_to_dma_framebuffer_set,
-                 vga_start           => slave_to_controller_start,
-                 vga_stop            => slave_to_controller_stop,
-                 vga_vsync           => controller_to_slave_vsync,
-                 as_address          => as_address,
-                 as_byteenable       => as_byteenable,
-                 as_read             => as_read,
-                 as_readdata         => as_readdata,
-                 as_write            => as_write,
-                 as_writedata        => as_writedata);
+	vga_slave_inst : component vga_slave
+		port map(clk                 => clk,
+			     rst_n               => rst_n,
+			     vsync_irq           => vsync_irq,
+			     framebuffer_address => slave_to_dma_framebuffer_address,
+			     framebuffer_set     => slave_to_dma_framebuffer_set,
+			     vga_start           => slave_to_controller_start,
+			     vga_stop            => slave_to_controller_stop,
+			     vga_vsync           => controller_to_slave_vsync,
+			     as_address          => as_address,
+			     as_byteenable       => as_byteenable,
+			     as_read             => as_read,
+			     as_readdata         => as_readdata,
+			     as_write            => as_write,
+			     as_writedata        => as_writedata);
 
-    vga_dma_inst : component vga_dma
-        port map(clk                 => clk,
-                 rst_n               => rst_n,
-                 framebuffer_address => slave_to_dma_framebuffer_address,
-                 framebuffer_set     => slave_to_dma_framebuffer_set,
-                 dma_start_fetching  => controller_to_dma_start_fetching,
-                 fifo_clr            => dma_to_fifo_clr,
-                 fifo_data           => dma_to_fifo_data,
-                 fifo_write          => dma_to_fifo_write,
-                 fifo_full           => fifo_to_dma_full,
-                 fifo_used           => fifo_to_dma_used,
-                 am_address          => am_address,
-                 am_byteenable       => am_byteenable,
-                 am_read             => am_read,
-                 am_burstcount       => am_burstcount,
-                 am_readdata         => am_readdata,
-                 am_waitrequest      => am_waitrequest,
-                 am_readdatavalid    => am_readdatavalid);
+	vga_dma_inst : component vga_dma
+		port map(clk                 => clk,
+			     rst_n               => rst_n,
+			     framebuffer_address => slave_to_dma_framebuffer_address,
+			     framebuffer_set     => slave_to_dma_framebuffer_set,
+			     dma_start_fetching  => controller_to_dma_start_fetching,
+			     fifo_clr            => dma_to_fifo_clr,
+			     fifo_data           => dma_to_fifo_data,
+			     fifo_write          => dma_to_fifo_write,
+			     fifo_half_full      => fifo_to_dma_used(5),
+			     am_address          => am_address,
+			     am_byteenable       => am_byteenable,
+			     am_read             => am_read,
+			     am_burstcount       => am_burstcount,
+			     am_readdata         => am_readdata,
+			     am_waitrequest      => am_waitrequest,
+			     am_readdatavalid    => am_readdatavalid);
 
-    vga_fifo_inst : component vga_fifo
-        port map(aclr    => dma_to_fifo_clr,
-                 data    => dma_to_fifo_data,
-                 rdclk   => pixel_clk,
-                 rdreq   => controller_to_fifo_read,
-                 wrclk   => clk,
-                 wrreq   => dma_to_fifo_write,
-                 q       => fifo_to_controller_data,
-                 rdempty => fifo_to_controller_empty,
-                 wrfull  => fifo_to_dma_full,
-                 wrusedw => fifo_to_dma_used);
+	vga_fifo_inst : component vga_fifo
+		port map(aclr    => dma_to_fifo_clr,
+			     data    => dma_to_fifo_data,
+			     rdclk   => pixel_clk,
+			     rdreq   => controller_to_fifo_read,
+			     wrclk   => clk,
+			     wrreq   => dma_to_fifo_write,
+			     q       => fifo_to_controller_data,
+			     rdempty => fifo_to_controller_empty,
+			     wrfull  => fifo_to_dma_full,
+			     wrusedw => fifo_to_dma_used);
 
-    vga_controller_inst : component vga_controller
-        port map(pixel_clk          => pixel_clk,
-                 rst_n              => rst_n,
-                 start              => slave_to_controller_start,
-                 stop               => slave_to_controller_stop,
-                 dma_start_fetching => controller_to_dma_start_fetching,
-                 vsync              => controller_to_slave_vsync,
-                 fifo_empty         => fifo_to_controller_empty,
-                 fifo_read          => controller_to_fifo_read,
-                 fifo_data          => fifo_to_controller_data,
-                 vga_r              => vga_r,
-                 vga_g              => vga_g,
-                 vga_b              => vga_b,
-                 vga_clk            => vga_clk,
-                 vga_sync_n         => vga_sync_n,
-                 vga_blank_n        => vga_blank_n,
-                 vga_vs             => vga_vs,
-                 vga_hs             => vga_hs);
+	vga_controller_inst : component vga_controller
+		port map(pixel_clk          => pixel_clk,
+			     rst_n              => rst_n,
+			     start              => slave_to_controller_start,
+			     stop               => slave_to_controller_stop,
+			     dma_start_fetching => controller_to_dma_start_fetching,
+			     vsync              => controller_to_slave_vsync,
+			     fifo_empty         => fifo_to_controller_empty,
+			     fifo_read          => controller_to_fifo_read,
+			     fifo_data          => fifo_to_controller_data,
+			     vga_r              => vga_r,
+			     vga_g              => vga_g,
+			     vga_b              => vga_b,
+			     vga_clk            => vga_clk,
+			     vga_sync_n         => vga_sync_n,
+			     vga_blank_n        => vga_blank_n,
+			     vga_vs             => vga_vs,
+			     vga_hs             => vga_hs);
 
 end architecture structural;

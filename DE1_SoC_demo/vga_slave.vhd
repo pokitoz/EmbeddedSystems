de034f6b2aaa194ae2391afb60886c1fe2e716ff
Andy Roulin <andy.roulin@epfl.ch>
2015-05-10 11:15:43 +0200

    vga_slave implementation
    Only irq handling is missing

--- DE1_SoC_demo/vga_slave.vhd
+++ DE1_SoC_demo/vga_slave.vhd
@@ -8,29 +8,94 @@ use ieee.numeric_std.all;
 --     010: Vsync irq, read to assert (R)
 
 entity vga_slave is
-    port(
-        clk                 : in  std_logic;
-        rst_n               : in  std_logic;
+	port(
+		clk                 : in  std_logic;
+		rst_n               : in  std_logic;
 
-        -- Vsync Interrupt
-        vsync_irq           : out std_logic;
+		-- Vsync Interrupt
+		vsync_irq           : out std_logic;
 
-        -- DMA Signals
-        framebuffer_address : out  std_logic_vector(31 downto 0);
-        framebuffer_set     : out  std_logic;
+		-- DMA Signals
+		framebuffer_address : out std_logic_vector(31 downto 0);
+		framebuffer_set     : out std_logic;
 
-        -- VGA Controller signals
-        vga_start           : out std_logic;
-        vga_stop            : out std_logic;
-        vga_vsync           : in std_logic;
+		-- VGA Controller signals
+		vga_start           : out std_logic;
+		vga_stop            : out std_logic;
+		vga_vsync           : in  std_logic;
 
-        -- Avalon 32-bit Slave Interface
-        -- 8 memory-mapped registers
-        as_address          : in  std_logic_vector(2 downto 0);
-        as_byteenable       : in  std_logic_vector(3 downto 0);
-        as_read             : in  std_logic;
-        as_readdata         : out std_logic_vector(31 downto 0);
-        as_write            : in  std_logic;
-        as_writedata        : in  std_logic_vector(31 downto 0)
-    );
+		-- Avalon 32-bit Slave Interface
+		-- 8 memory-mapped registers
+		as_address          : in  std_logic_vector(2 downto 0);
+		as_byteenable       : in  std_logic_vector(3 downto 0);
+		as_read             : in  std_logic;
+		as_readdata         : out std_logic_vector(31 downto 0);
+		as_write            : in  std_logic;
+		as_writedata        : in  std_logic_vector(31 downto 0)
+	);
 end entity vga_slave;
+
+architecture rtl of vga_slave is
+	signal framebuffer_address_reg : std_logic_vector(31 downto 0);
+	signal command_reg             : std_logic_vector(31 downto 0);
+
+begin
+	pr_read : process(clk, rst_n) is
+	begin
+		if rst_n = '0' then
+			as_readdata <= (others => '0');
+		elsif rising_edge(clk) then
+			as_readdata <= (others => '0');
+			if (as_read = '1') then
+				case as_address is
+					-- Reading framebuffer address
+					when "000" =>
+						as_readdata <= framebuffer_address_reg;
+					-- Reading command register
+					when "001" =>
+						as_readdata <= command_reg;
+					when others => null;
+				end case;
+
+			end if;
+		end if;
+	end process pr_read;
+
+	pr_write : process(clk, rst_n) is
+	begin
+		if rst_n = '0' then
+			framebuffer_address_reg <= (others => '0');
+			command_reg             <= (others => '0');
+			framebuffer_address     <= (others => '0');
+			framebuffer_set         <= '0';
+			vga_start               <= '0';
+			vga_stop                <= '0';
+		elsif rising_edge(clk) then
+			framebuffer_address <= (others => '0');
+			framebuffer_set     <= '0';
+			vga_start           <= '0';
+			vga_stop            <= '0';
+			if (as_write = '1') then
+				case as_address is
+					-- Writing framebuffer address
+					when "000" =>
+						framebuffer_address_reg <= as_writedata;
+						framebuffer_address     <= as_writedata;
+						framebuffer_set         <= '1';
+					-- Writing command register
+					when "001" =>
+						command_reg <= (31 downto 1 => '0') & as_writedata(0);
+						if (as_writedata(0) = '1') then -- start
+							vga_start <= '1';
+						else            -- stop
+							vga_stop <= '1';
+						end if;
+					when others => null;
+				end case;
+
+			end if;
+		end if;
+	end process pr_write;
+
+end architecture rtl;
+
--- DE1_SoC_demo/vga_slave_tb.vhd
+++ DE1_SoC_demo/vga_slave_tb.vhd
@@ -6,97 +6,130 @@ entity vga_slave_tb is
 end entity vga_slave_tb;
 
 architecture testbench of vga_slave_tb is
-    constant period : time      := 20 ns;
-    signal clk      : std_logic := '0';
-    signal rst_n    : std_logic := '0';
-    signal stop     : boolean   := false;
+	constant PERIOD : time      := 20 ns;
+	signal clk      : std_logic := '0';
+	signal rst_n    : std_logic := '0';
+	signal stop     : boolean   := false;
 
-    component vga_slave
-        port(clk                 : in  std_logic;
-             rst_n               : in  std_logic;
-             vsync_irq           : out std_logic;
-             framebuffer_address : out std_logic_vector(31 downto 0);
-             framebuffer_set     : out std_logic;
-             vga_start           : out std_logic;
-             vga_stop            : out std_logic;
-             vga_vsync           : in  std_logic;
-             as_address          : in  std_logic_vector(2 downto 0);
-             as_byteenable       : in  std_logic_vector(3 downto 0);
-             as_read             : in  std_logic;
-             as_readdata         : out std_logic_vector(31 downto 0);
-             as_write            : in  std_logic;
-             as_writedata        : in  std_logic_vector(31 downto 0));
-    end component vga_slave;
+	component vga_slave
+		port(clk                 : in  std_logic;
+			 rst_n               : in  std_logic;
+			 vsync_irq           : out std_logic;
+			 framebuffer_address : out std_logic_vector(31 downto 0);
+			 framebuffer_set     : out std_logic;
+			 vga_start           : out std_logic;
+			 vga_stop            : out std_logic;
+			 vga_vsync           : in  std_logic;
+			 as_address          : in  std_logic_vector(2 downto 0);
+			 as_byteenable       : in  std_logic_vector(3 downto 0);
+			 as_read             : in  std_logic;
+			 as_readdata         : out std_logic_vector(31 downto 0);
+			 as_write            : in  std_logic;
+			 as_writedata        : in  std_logic_vector(31 downto 0));
+	end component vga_slave;
 
-    signal vsync_irq           : std_logic;
-    signal framebuffer_address : std_logic_vector(31 downto 0);
-    signal framebuffer_set     : std_logic;
-    signal vga_start           : std_logic;
-    signal vga_stop            : std_logic;
-    signal vga_vsync           : std_logic;
-    signal as_address          : std_logic_vector(2 downto 0);
-    signal as_byteenable       : std_logic_vector(3 downto 0) := "1111";
-    signal as_read             : std_logic;
-    signal as_readdata         : std_logic_vector(31 downto 0);
-    signal as_write            : std_logic;
-    signal as_writedata        : std_logic_vector(31 downto 0);
+	signal vsync_irq           : std_logic;
+	signal framebuffer_address : std_logic_vector(31 downto 0);
+	signal framebuffer_set     : std_logic;
+	signal vga_start           : std_logic;
+	signal vga_stop            : std_logic;
+	signal vga_vsync           : std_logic                     := '0';
+	signal as_address          : std_logic_vector(2 downto 0)  := "000";
+	signal as_byteenable       : std_logic_vector(3 downto 0)  := "1111";
+	signal as_read             : std_logic                     := '0';
+	signal as_readdata         : std_logic_vector(31 downto 0) := X"00000000";
+	signal as_write            : std_logic                     := '0';
+	signal as_writedata        : std_logic_vector(31 downto 0) := X"00000000";
+
+	constant REG_ADDRESS     : std_logic_vector(as_address'range)   := "000";
+	constant EXAMPLE_ADDRESS : std_logic_vector(as_writedata'range) := X"12345678";
+	constant REG_COMMAND     : std_logic_vector(as_address'range)   := "001";
+	constant COMMAND_START   : std_logic_vector(as_writedata'range) := X"00000001";
+	constant COMMAND_STOP    : std_logic_vector(as_writedata'range) := X"00000000";
 
 begin
-    clk   <= not clk after period / 2 when not stop else '0';
-    rst_n <= '1' after period when not stop else '1';
+	clk   <= not clk after PERIOD / 2 when not stop else '0';
+	rst_n <= '1' after PERIOD when not stop else '1';
 
-    duv : component vga_slave
-        port map(clk                 => clk,
-                 rst_n               => rst_n,
-                 vsync_irq           => vsync_irq,
-                 framebuffer_address => framebuffer_address,
-                 framebuffer_set     => framebuffer_set,
-                 vga_start           => vga_start,
-                 vga_stop            => vga_stop,
-                 vga_vsync           => vga_vsync,
-                 as_address          => as_address,
-                 as_byteenable       => as_byteenable,
-                 as_read             => as_read,
-                 as_readdata         => as_readdata,
-                 as_write            => as_write,
-                 as_writedata        => as_writedata);
+	duv : component vga_slave
+		port map(clk                 => clk,
+			     rst_n               => rst_n,
+			     vsync_irq           => vsync_irq,
+			     framebuffer_address => framebuffer_address,
+			     framebuffer_set     => framebuffer_set,
+			     vga_start           => vga_start,
+			     vga_stop            => vga_stop,
+			     vga_vsync           => vga_vsync,
+			     as_address          => as_address,
+			     as_byteenable       => as_byteenable,
+			     as_read             => as_read,
+			     as_readdata         => as_readdata,
+			     as_write            => as_write,
+			     as_writedata        => as_writedata);
 
-    test : process is
-        procedure write_register(
-            reg   : std_logic_vector(as_address'range);
-            value : std_logic_vector(as_writedata'range)) is
-        begin
-            as_address   <= reg;
-            as_write     <= '1';
-            as_writedata <= value;
-            wait for period;
-            as_address   <= (others => '0');
-            as_write     <= '0';
-            as_writedata <= (others => '0');
-        end procedure write_register;
+	test : process is
+		procedure write_register(
+			reg   : std_logic_vector(as_address'range);
+			value : std_logic_vector(as_writedata'range)) is
+		begin
+			as_address   <= reg;
+			as_write     <= '1';
+			as_writedata <= value;
+			wait for PERIOD;
+			as_address   <= (others => '0');
+			as_write     <= '0';
+			as_writedata <= (others => '0');
+		end procedure write_register;
 
-        procedure assert_read(
-            reg      : std_logic_vector(as_address'range);
-            expected : std_logic_vector(as_writedata'range)) is
-        begin
-            as_address <= reg;
-            as_read    <= '1';
-            wait for period;
-            as_read    <= '0';
-            as_address <= (others => '0');
-            wait until rising_edge(clk);
-            assert as_readdata = expected report "Read: expected " & integer'image(to_integer(unsigned(expected))) & " but was " & integer'image(to_integer(unsigned(as_readdata))) severity error;
-            wait for period / 4;
-        end procedure assert_read;
+		procedure assert_read(
+			reg      : std_logic_vector(as_address'range);
+			expected : std_logic_vector(as_writedata'range)) is
+		begin
+			as_address <= reg;
+			as_read    <= '1';
+			wait for PERIOD;
+			as_read    <= '0';
+			as_address <= (others => '0');
+			wait until rising_edge(clk);
+			assert as_readdata = expected report "Read: expected " & integer'image(to_integer(unsigned(expected))) & " but was " & integer'image(to_integer(unsigned(as_readdata))) severity error;
+			wait for PERIOD / 4;
+		end procedure assert_read;
 
-    begin
-        wait for 2 * period;
-        wait for (period / 2) + (period / 4);
+	begin
+		wait for 2 * PERIOD;
+		wait for (PERIOD / 2) + (PERIOD / 4);
 
-        write_register("000", X"12345678");
+		-- Write the address should force the slave to write it to the DMA unit
+		write_register(REG_ADDRESS, EXAMPLE_ADDRESS);
+		wait until rising_edge(clk);
+		assert framebuffer_address = EXAMPLE_ADDRESS report "Framebuffer address: expected " & integer'image(to_integer(unsigned(EXAMPLE_ADDRESS))) & " but was " & integer'image(to_integer(unsigned(framebuffer_address))) severity error;
+		assert framebuffer_set = '1' report "Framebuffer_set is not set" severity error;
+		wait for PERIOD / 4;
+		assert_read(REG_ADDRESS, X"12345678");
 
-        stop <= true;
-        wait;
-    end process test;
+		-- Command register should be cleared initially
+		assert_read(REG_COMMAND, COMMAND_STOP);
+		
+		-- Start VGA system
+		assert vga_start = '0' report "vga_start not cleared before use" severity error;
+		write_register(REG_COMMAND, COMMAND_START);
+		wait until rising_edge(clk);
+		assert vga_start = '1' report "vga_start not set" severity error;
+		wait for PERIOD / 4;
+		assert vga_start = '0' report "vga_start not cleared after use" severity error;
+		assert_read(REG_COMMAND, COMMAND_START);
+		
+		-- Stop VGA system
+		assert vga_stop = '0' report "vga_stop not cleared before use" severity error;
+		write_register(REG_COMMAND, COMMAND_STOP);
+		wait until rising_edge(clk);
+		assert vga_stop = '1' report "vga_stop not set" severity error;
+		wait for PERIOD / 4;
+		assert vga_stop = '0' report "vga_stop not cleared after use" severity error;
+		assert_read(REG_COMMAND, COMMAND_STOP);
+
+		stop <= true;
+		wait;
+	end process test;
 
 end architecture testbench;
